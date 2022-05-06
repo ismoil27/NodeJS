b84ecc19b7ac5f89836fa788ab04ad9f3c0cf723
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-05-06 17:41:25 +0900

    created first simple nodejs server of mine

--- logger.js
+++ logger.js
@@ -21,20 +21,20 @@
 // let updatedArray = [...new Set(arr)];
 // console.log(updatedArray);
 
-const obj = {
-  a: 1,
-  b: 2,
-};
+// const obj = {
+//   a: 1,
+//   b: 2,
+// };
 
-const obj2 = {
-  c: 3,
-  d: 4,
-};
-const obj3 = {
-  e: 5,
-  f: 6,
-};
+// const obj2 = {
+//   c: 3,
+//   d: 4,
+// };
+// const obj3 = {
+//   e: 5,
+//   f: 6,
+// };
 
-const allObj = Object.assign(obj, obj2, obj3); //This is another way of adding multiple object into one object
+// const allObj = Object.assign(obj, obj2, obj3); //This is another way of adding multiple object into one object
 
-console.log(allObj);
+// console.log(allObj);
