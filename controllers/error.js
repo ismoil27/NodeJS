1fc758490395a32f6b9d989405540c440b84d5da
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-17 16:18:44 +0900

    some changes

--- controllers/error.js
+++ controllers/error.js
@@ -1,9 +1,3 @@
 exports.get404 = (req, res, next) => {
-  res
-    .status(404)
-    .render("404", {
-      pageTitle: "Page Not Found",
-      layout: false,
-      path: "/404",
-    });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
--- models/product.js
+++ models/product.js
@@ -18,14 +18,18 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        // console.log(err);
+        console.log(err);
       });
     });
   }
