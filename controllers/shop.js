a861a492fb47991be55239b57960254c046ebe28
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-29 17:51:05 +0900

    added some changes;

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,7 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  res.redirect("/");
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
